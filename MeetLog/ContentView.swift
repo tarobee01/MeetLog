8b93ad8ee5e2765fee49450f3678bc78a76e5420
tarobee01 <monchichi0124@gmail.com>
2024-02-21 15:19:19 +0900

    Initial Commit

---
+++ MeetLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetLog
+//
+//  Created by 武林慎太郎 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeetLog/MeetLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetLogApp.swift
+//  MeetLog
+//
+//  Created by 武林慎太郎 on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MeetLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
